33cc476e226a22c49e9b8030eab129a7d54b7db1
TheQuestCrafter <thequestcrafter@gmail.com>
2019-03-13 19:52:07 -0500

    Main Menu Implementation

---
+++ Assets/Scripts/ActivateOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActivateOnClick : MonoBehaviour
+{
+    [Tooltip("Int for sceneIndex to new scene")]
+    [SerializeField]
+    private int sceneSelected = 1;
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(sceneSelected);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.transform.RotateAround(transform.position, Vector3.up, Time.deltaTime);
+    }
+}
