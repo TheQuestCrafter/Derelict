f696ddf1bc9e898c52332c09acb7c3ec8fca625b
TheQuestCrafter <thequestcrafter@gmail.com>
2019-04-04 02:51:52 -0500

    Basic Inventory, No Menu

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,12 +8,20 @@ public class Door : InteractiveObject
     [Tooltip("Check this box to lock the door.")]
     [SerializeField]
     private bool isLocked = false;
+
+    [Tooltip("Text that is displayed when interacted with and the door is locked.")]
     [SerializeField]
     private string lockedDoorText = "Locked";
+
+    [Tooltip("Text that is displayed when interacted with and the door is unlocked.")]
     [SerializeField]
     private string unlockedDoorText = "Opened Door";
+
+    [Tooltip("Audioclip for locked door.")]
     [SerializeField]
     private AudioClip lockedDoorSound;
+
+    [Tooltip("Audioclip for unlocked door.")]
     [SerializeField]
     private AudioClip unlockedDoorSound;
 
--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -6,4 +6,34 @@ public class InventoryObject : InteractiveObject
 {
     // TODO: Add long description field
     // TODO: Add Icon Field
+
+    [Tooltip("The text that will display in the UI when the player looks at it in the menu.")]
+    [SerializeField]
+    private string objectName = nameof(InventoryObject);
+    
+    private Renderer objRenderer;
+    private Collider objCollider;
+
+    private void Start()
+    {
+        objCollider = this.GetComponent<Collider>();
+        objRenderer = this.GetComponent<Renderer>();
+    }
+
+    public InventoryObject()
+    {
+        displayText = $"Take {objectName}";
+    }
+    /// <summary>
+    /// When the player interacts with the object it needs to do two things.
+    /// 1. Add inventory object to the player inventory script
+    /// 2. Remove the object from the game world
+    /// </summary>
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        PlayerInventory.InventoryObjects.Add(this);
+        objCollider.enabled = false;
+        objRenderer.enabled = false;        
+    }
 }
